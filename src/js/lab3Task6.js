c7fe680711e5329306481b97cd98769bdee6e03d
AndriykoF <fesiukandrey@gmail.com>
2023-04-01 22:27:57 +0300

    Fixed task 6

--- src/js/lab3Task6.js
+++ src/js/lab3Task6.js
@@ -1,13 +1,28 @@
 import  {finalObject} from "./lab3.js";
 
-function PercentageSearch(arrayObject, parametr, value){
+function PercentageSearch(arrayObject, parametr, value,range){
     let count=0;
-    for(let obj of arrayObject)
-    {
-        if(obj[parametr]===value){
-            count++;
+    if(parametr != "b_day"&&parametr != "age"){
+        for(let obj of arrayObject){
+            if(obj[parametr]>value){
+                    count++;
+            }
+        }
+    }
+    else{
+        for(let obj of arrayObject){
+            if(range==="greater"){
+                if(obj[parametr]>=value){
+                    count++;
+                }
+            }
+            else{
+                if(obj[parametr]<value){
+                    count++;
+                }
+            }
         }
     }
     return (count/arrayObject.length)*100;
 }
-//console.log(PercentageSearch(finalObject, 'gender','Female'));
+//console.log(PercentageSearch(finalObject, 'age','75','less'));
