3e7a1f4cf3450e368b2d7b180d963f24ef5f8584
AndriykoF <fesiukandrey@gmail.com>
2023-04-01 21:47:53 +0300

    Fixed task 4

--- src/js/lab3Task4.js
+++ src/js/lab3Task4.js
@@ -1,9 +1,6 @@
 import  {finalObject} from "./lab3.js";
 
 function SortArray(arrayObjects,sortby,sortDirection){
-    if(typeof arrayObjects[0][sortby] === "string")
-        arrayObjects.sort((a, b) => a[sortby] > b[sortby] ? 1 : -1);
-    else
         arrayObjects.sort((a, b) => a[sortby] > b[sortby] ? 1 : -1);
     if(sortDirection === 'desc')
         arrayObjects.reverse();
